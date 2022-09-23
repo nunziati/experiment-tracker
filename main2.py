ba5fa0aa15d9c9d5fe688f7b209990fd22756fb8
Giacomo Nunziati <nunziati@kronos.diism.unisi.it>
2022-09-23 13:26:35 +0200

    recover last training

--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@ from model import Simple_MLP
 from experiment import build_experiment
 from itertools import product
 import matplotlib.pyplot as plt
+import os
 
 
 """
@@ -13,11 +14,19 @@ optimizer_values = ["adam", "sgd"]
 split_mnist
 sgd
 
-10:     12411.pts-51.maxi
-30:     12119.pts-51.maxi running
-100:    13110.pts-51.maxi running
-300:    13503.pts-51.maxi running
-1000:   14236.pts-51.maxi
+10:     12411.pts-51.maxi  finito
+30:     12119.pts-51.maxi  finito
+100:    29008.pts-9.kronos finito
+300:    29377.pts-9.kronos finito
+
+
+cifar10
+sgd
+
+10:     12411.pts-51.maxi  
+30:     12119.pts-51.maxi  
+100:    29008.pts-9.kronos 
+300:    29377.pts-9.kronos 
 """
 
 """
@@ -36,7 +45,7 @@ batch_size_values = [1, 10, 100]
 memory_units_values = [3, 10, 30, 100]
 delta_values = [0.01, 0.03, 0.1, 0.3]
 
-hidden_unit_values = [100]
+hidden_unit_values = [1000]
 
 path = f"experiments/online_mlp_cmm_{datasets[0]}_{optimizer_values[0]}_hu{hidden_unit_values[0]}/"
 
@@ -80,37 +89,41 @@ for dataset_name in datasets:
 
         cmm_args={'base_m': memory_units, 'delta': delta}
 
-        model = Simple_MLP(input_dim, hidden_units, output_dim, dropout=0.0, output="logits", cmm=True, cmm_args=cmm_args)
+        if os.path.isdir(f"{path}exp{str(exp_id).rjust(4, '0')}") and os.path.isfile(f"{path}exp{str(exp_id).rjust(4, '0')}/results/holdout/accuracy.txt"):
+            with open(f"{path}exp{str(exp_id).rjust(4, '0')}/results/holdout/accuracy.txt", "r") as f:
+                accuracy = f.read()
+        else:
+            model = Simple_MLP(input_dim, hidden_units, output_dim, dropout=0.0, output="logits", cmm=True, cmm_args=cmm_args)
 
-        config = dict(
-            path = path,
-            name = f"exp{str(exp_id).rjust(4, '0')}",
-            model = model,
-            type = "online",
-            dataset = dataset_name,
-            sorted = True,
-            epoch_number = 1,
-            loss_function_name = "cross_entropy_loss",
-            optimizer_name = optimizer,
-            optimizer_args = dict(
-                lr = learning_rate,
-                weight_decay = 0.0001
-            ),
-            batch_size = batch_size,
-            metrics = "accuracy",
-            device = "cuda:0",
-            dataloader_args = dict(
-                num_workers = 4
-            ),
-            validation = 0.2,
-            evaluation_batch_size = 100,
-            evaluation_step = "task"
-        )
+            config = dict(
+                path = path,
+                name = f"exp{str(exp_id).rjust(4, '0')}",
+                model = model,
+                type = "online",
+                dataset = dataset_name,
+                sorted = True,
+                epoch_number = 1,
+                loss_function_name = "cross_entropy_loss",
+                optimizer_name = optimizer,
+                optimizer_args = dict(
+                    lr = learning_rate,
+                    weight_decay = 0.0001
+                ),
+                batch_size = batch_size,
+                metrics = "accuracy",
+                device = "cuda:1",
+                dataloader_args = dict(
+                    num_workers = 4
+                ),
+                validation = 0.2,
+                evaluation_batch_size = 100,
+                evaluation_step = "task"
+            )
 
-        experiment = build_experiment(config)
-        experiment.run()
+            experiment = build_experiment(config)
+            experiment.run()
 
-        accuracy = experiment.results["holdout"]["accuracy"]
+            accuracy = experiment.results["holdout"]["accuracy"]
 
         results_file.append(f"obj1_{dataset_name}_exp{str(exp_id).rjust(3, '0')},{dataset_name},{hidden_units},{learning_rate},{optimizer},{batch_size},{memory_units},{delta},{accuracy}\n")
 
